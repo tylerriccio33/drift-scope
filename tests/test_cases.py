22d8fecaee3601bb042ca290ecfaadbf1355508a
Tyler Riccio <tylerriccio8@gmail.com>
2025-02-28 21:58:32 -0500

    Refactor test casing

--- tests/test_cases.py
+++ tests/test_cases.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
 import contextlib
-import itertools
-from typing import TYPE_CHECKING, NamedTuple, cast
+import copy
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, cast
 
 import duckdb
 import polars as pl
@@ -10,15 +11,16 @@ import psycopg2
 import pytest
 from testcontainers.postgres import PostgresContainer
 
-from drift_scope._sql import SQLConnections
+from drift_scope._sql import SQL_CONNECTIONS, SQLConnections
+from drift_scope.dataframe import DataFrameCompare
 from drift_scope.results import FreqResults
 from drift_scope.sql import SQLComparator
 
 if TYPE_CHECKING:
     from collections.abc import Callable, Generator
-    from typing import Any
+    from typing import Any, Literal
 
-    from drift_scope._sql import SQL_CONNECTIONS
+    from drift_scope.base import BaseComparator
 
     type GenReturn = Generator[Any, None, None]
     type ConYielder = Callable[[], GenReturn]
@@ -39,59 +41,84 @@ def _get_duckdb() -> GenReturn:
         yield con
 
 
-class _Args(NamedTuple):  # TODO: Change the name of this
-    con_type: SQL_CONNECTIONS
-    yielder: Any
+@dataclass
+class _Args:  # TODO: Change the name of this
+    con_type: SQL_CONNECTIONS | Literal["NARWHALS"]  # TODO : rename to engine
+    comparator: BaseComparator
+    yielder: Any = contextlib.nullcontext  # TODO: Need better name
     work_schema: str | None = None
 
 
 args: tuple[_Args, ...] = (
-    _Args(con_type="DUCKDB", yielder=_get_duckdb),
-    _Args(con_type="PSYCOPG2", yielder=_get_postgres),
+    _Args(con_type="NARWHALS", comparator=DataFrameCompare),
+    _Args(con_type="DUCKDB", comparator=SQLComparator, yielder=_get_duckdb),
+    _Args(con_type="PSYCOPG2", comparator=SQLComparator, yielder=_get_postgres),
 )
 
 schema_opts = (None, "fooschema")
-expanded_args = tuple(
-    _Args(con_type=arg.con_type, yielder=arg.yielder, work_schema=schema)
-    for arg, schema in itertools.product(args, schema_opts)
-)
+expanded_args: list[_Args] = []
+for arg in args:
+    if arg.con_type in SQL_CONNECTIONS:
+        for schema in schema_opts:  # expand by schema
+            args_copy: _Args = copy.copy(arg)
+            args_copy.work_schema = schema
+            expanded_args.append(args_copy)
+        continue
+    expanded_args.append(arg)
 
 
 @pytest.mark.parametrize("arg", expanded_args)
 def test_manual1(arg: _Args) -> None:
     """Test categorical comparison case."""
     with arg.yielder() as con:
-        protocol = SQLConnections[arg.con_type].value
+        try:
+            protocol = SQLConnections[arg.con_type].value
+            is_sql: bool = True  # TODO: again, this is a terrible solution
+        except KeyError:
+            is_sql: bool = False  # type: ignore[no-redef]
+            table1 = pl.DataFrame(
+                {"city": ["New York", "Los Angeles", "Chicago"], "state": ["NY", "CA", "IL"]}
+            )
+            table2 = pl.DataFrame(
+                {"city": ["New York", "Phoenix", "Philadelphia"], "state": ["NY", "AZ", "PA"]}
+            )
+            comp = arg.comparator(table1, table2)
+        else:
+            tables_at_start: tuple[str, ...] = protocol.get_tables(con)
 
-        tables_at_start: tuple[str, ...] = protocol.get_tables(con)
+            con.execute("BEGIN TRANSACTION;")
 
-        con.execute("BEGIN TRANSACTION;")
+            if arg.work_schema:
+                con.execute(f"CREATE SCHEMA {arg.work_schema}")
 
-        if arg.work_schema:
-            con.execute(f"CREATE SCHEMA {arg.work_schema}")
+            con.execute("CREATE TABLE table1 (city VARCHAR, state VARCHAR)")
+            con.execute(
+                "INSERT INTO table1 VALUES ('New York', 'NY'), ('Los Angeles', 'CA'), ('Chicago', 'IL')"
+            )
 
-        con.execute("CREATE TABLE table1 (city VARCHAR, state VARCHAR)")
-        con.execute(
-            "INSERT INTO table1 VALUES ('New York', 'NY'), ('Los Angeles', 'CA'), ('Chicago', 'IL')"
-        )
+            con.execute("CREATE TABLE table2 (city VARCHAR, state VARCHAR)")
+            con.execute(
+                "INSERT INTO table2 VALUES ('New York', 'NY'), ('Phoenix', 'AZ'), ('Philadelphia', 'PA')"
+            )
 
-        con.execute("CREATE TABLE table2 (city VARCHAR, state VARCHAR)")
-        con.execute(
-            "INSERT INTO table2 VALUES ('New York', 'NY'), ('Phoenix', 'AZ'), ('Philadelphia', 'PA')"
-        )
+            con.execute("COMMIT;")  # ensure the tables exist before the comp
 
-        con.execute("COMMIT;")  # ensure the tables exist before the comp
+            comp = arg.comparator(
+                df1="table1",
+                df2="table2",
+                con=con,
+                con_type=arg.con_type,
+                work_schema=arg.work_schema,
+            )
 
-        comp = SQLComparator(
-            df1="table1", df2="table2", con=con, con_type=arg.con_type, work_schema=arg.work_schema
-        )
         comp.comp_freq(vars=("city", "state"))
 
-        ## Assert Integrity of Connection:
-        tables: tuple[str, ...] = protocol.get_tables(con)
-        msg = "Additional tables were persisted against the original connection."
-        new_tables = set(tables) - set(tables_at_start)
-        assert set(new_tables) == {"table1", "table2"}, msg
+        if is_sql:
+            ## Assert Integrity of Connection:
+            tables: tuple[str, ...] = protocol.get_tables(con)
+            msg = "Additional tables were persisted against the original connection."
+            new_tables = set(tables) - set(tables_at_start)
+            assert set(new_tables) == {"table1", "table2"}, msg
 
     ## Baseline Assertions:
     msg = "Results must be filled out"
